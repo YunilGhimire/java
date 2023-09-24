de7b54ba69c8b0907089447313d7941394a12d3b
YunilGhimire <yunilghimire@gmail.com>
2023-09-24 20:40:01 +0545

    24th Sept

--- src/Class5/FizzBuzz.java
+++ src/Class5/FizzBuzz.java
@@ -4,12 +4,12 @@ public class FizzBuzz {
     public static void main(String[] args) {
         for(int i = 1; i<=100; i++ ){
             if(i%3==0 && i%5==0) {
-                System.out.println("FizzBuzz");
+                System.out.println(i + " FizzBuzz");
             } else if (i%3==0) {
-                System.out.println("Fizz");
+                System.out.println(i + " Fizz");
 
             }else if (i%5==0){
-                System.out.println("Buzz");
+                System.out.println(i + " Buzz");
             }else {
                 System.out.println("Invalid input");
             }
--- src/Class5/Percentage_and_Grade.java
+++ src/Class5/Percentage_and_Grade.java
@@ -1,2 +1,36 @@
-package Class5;public class Percentage_and_Grade {
+package Class5;
+
+import java.util.Scanner;
+
+public class Percentage_and_Grade {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of subjects: ");
+        int numSubjects = sc.nextInt();
+        double totalMarks = 0;
+        for (int i = 1; i <= numSubjects; i++) {
+            System.out.print("Enter marks for Subject " + i + ": ");
+            double marks = sc.nextDouble();
+            if (marks < 0 || marks > 100) {
+                System.out.println("Invalid input");
+                return;
+            }
+            totalMarks += marks;
+        }
+        double percentage = (totalMarks / (numSubjects * 100)) * 100;
+        System.out.println("Percentage: " + percentage + "%");
+        char grade;
+        if (percentage >= 90) {
+            grade = 'A';
+        } else if (percentage >= 80) {
+            grade = 'B';
+        } else if (percentage >= 70) {
+            grade = 'C';
+        } else if (percentage >= 60) {
+            grade = 'D';
+        } else {
+            grade = 'F';
+        }
+        System.out.println("Grade: " + grade);
+    }
 }
